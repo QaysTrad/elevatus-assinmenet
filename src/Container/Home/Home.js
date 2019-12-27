1accdec4648765f155ade8645b650d3473c72c6a
Qays Trad <qays.t@wethemakers.dev>
2019-12-27 21:49:59 +0200

    create simple home page

--- src/Container/Home/Home.js
+++ src/Container/Home/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Logo from "../../Assets/Images/logo_2.png";
 
-const Home = () => (
-  <div>
-    <>Home</>
+const Home = ({ history }) => (
+  <div className="home-container">
+    <div>
+      <img src={Logo} alt="" />
+    </div>
+    <div className="home-text">Welcome Elevatus Team</div>
+    <div className="home-text">Press Next to start</div>
+    <div className="button home-button" onClick={() => history.push("upload")}>
+      Next
+    </div>
   </div>
 );
 

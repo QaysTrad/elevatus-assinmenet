52b421d4dbdba534da116278b4ebe5e8d01ea89c
Qays Trad <qays.t@wethemakers.dev>
2019-12-30 00:41:27 +0200

    changes on search and dropdown onchange

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -2,22 +2,30 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { DropdownButton, Dropdown } from "react-bootstrap";
 
-const DropdownCustom = ({ icon, list, name }) => (
+const DropdownCustom = ({ icon, list, name, title, onChange, value }) => (
   <DropdownButton
     id={name}
     title={
       <div className="dropdown-content">
-        <FontAwesomeIcon icon={icon} />
-        <div className="dropdown-text heavy-text">{name}</div>
+        {value ? "" : <FontAwesomeIcon icon={icon} />}
+        <div className="dropdown-text heavy-text">{value ? value : name}</div>
       </div>
     }
+    onSelect={eventKey => {
+      onChange(prevState => ({
+        ...prevState,
+        [title]: {
+          label: eventKey,
+          value: list[eventKey]
+        }
+      }));
+    }}
   >
     {list.map((lists, index) => (
-      <Dropdown.Item key={index} eventKey={index}>
+      <Dropdown.Item key={index} eventKey={index} value={lists}>
         {lists}
       </Dropdown.Item>
     ))}
-    <Dropdown.Divider />
   </DropdownButton>
 );
 
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -2,9 +2,16 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const Search = ({ name }) => (
+const Search = ({ name, onChange, title, value }) => (
   <div className="search-content">
-    <input className="search-input" placeholder={name} />
+    <input
+      type="text"
+      className="search-input"
+      placeholder={title}
+      onChange={onChange}
+      name={name}
+      value={value}
+    />
     <div className="search-icon">
       <FontAwesomeIcon icon={faSearch} />
     </div>

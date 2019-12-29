a63c3b9dfe2b157136f496f1b923c44fb8253cf1
Qays Trad <qays.t@wethemakers.dev>
2019-12-30 00:40:56 +0200

    create applicante data

---
+++ src/Container/ShowCV/Applicante.js
@@ -0,0 +1,183 @@
+import SmallPic from "../../Assets/Images/35295307.jpeg";
+
+export default [
+  {
+    logo: SmallPic,
+    name: "Mikel lang",
+    address: "2301 market Street, Philadelphia, PA USA",
+    Position: {
+      pos: "Director",
+      name: "lorem lpsum",
+      year: {
+        from: "Aug 2018",
+        to: "Present"
+      }
+    },
+    university: {
+      major: "Ceramics",
+      name: "Lorem lpsum",
+      year: { start: "2012", end: "2019" }
+    },
+    email: "qaystrad@gmail.com",
+    likes: [
+      "Photoshop",
+      "Illstrator",
+      "Coreldrew X5",
+      "Lerom",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum"
+    ]
+  },
+  {
+    logo: SmallPic,
+    name: "Deadpool",
+    address: "2301 market Street, Philadelphia, PA USA",
+    Position: {
+      pos: "Developer",
+      name: "lorem lpsum",
+      year: {
+        from: "Aug 2018",
+        to: "Present"
+      }
+    },
+    university: {
+      major: "Computer Science",
+      name: "Lorem lpsum",
+      year: { start: "2012", end: "2019" }
+    },
+    email: "qaystrad@gmail.com",
+    likes: [
+      "JavaScript",
+      "Nodejs",
+      "React",
+      "React Native",
+      "Nestjs",
+      "Graphql"
+    ]
+  },
+  {
+    logo: SmallPic,
+    name: "Mad Mid",
+    address: "2301 market Street, Philadelphia, PA USA",
+    Position: {
+      pos: "Designer",
+      name: "lorem lpsum",
+      year: {
+        from: "Aug 2018",
+        to: "Present"
+      }
+    },
+    university: {
+      major: "Geology",
+      name: "Lorem lpsum",
+      year: { start: "2012", end: "2019" }
+    },
+    email: "qaystrad@gmail.com",
+    likes: [
+      "Photoshop",
+      "Illstrator",
+      "Coreldrew X5",
+      "Lerom",
+      "Lerom lpsum",
+      "JavaScript",
+      "Nodejs",
+      "React",
+      "React Native",
+      "Nestjs",
+      "Graphql"
+    ]
+  },
+  {
+    logo: SmallPic,
+    name: "Lex Lo",
+    address: "2301 market Street, Philadelphia, PA USA",
+    Position: {
+      pos: "SEO",
+      name: "lorem lpsum",
+      year: {
+        from: "Aug 2018",
+        to: "Present"
+      }
+    },
+    university: {
+      major: "Information Systems",
+      name: "Lorem lpsum",
+      year: { start: "2012", end: "2019" }
+    },
+    email: "qaystrad@gmail.com",
+    likes: [
+      "Photoshop",
+      "Illstrator",
+      "Coreldrew X5",
+      "Lerom",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum"
+    ]
+  },
+  {
+    logo: SmallPic,
+    name: "Ahmad D",
+    address: "2301 market Street, Philadelphia, PA USA",
+    Position: {
+      pos: "STO",
+      name: "lorem lpsum",
+      year: {
+        from: "Aug 2018",
+        to: "Present"
+      }
+    },
+    university: {
+      major: "Computer Science",
+      name: "Lorem lpsum",
+      year: { start: "2012", end: "2019" }
+    },
+    email: "qaystrad@gmail.com",
+    likes: [
+      "Photoshop",
+      "Illstrator",
+      "Coreldrew X5",
+      "Lerom",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum"
+    ]
+  },
+  {
+    logo: SmallPic,
+    name: "Mohammad W",
+    address: "2301 market Street, Philadelphia, PA USA",
+    Position: {
+      pos: "Director",
+      name: "lorem lpsum",
+      year: {
+        from: "Aug 2018",
+        to: "Present"
+      }
+    },
+    university: {
+      major: "Computer Engineering",
+      name: "Lorem lpsum",
+      year: { start: "2012", end: "2019" }
+    },
+    email: "qaystrad@gmail.com",
+    likes: [
+      "Photoshop",
+      "Illstrator",
+      "Coreldrew X5",
+      "Lerom",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum",
+      "Lerom lpsum"
+    ]
+  }
+];
